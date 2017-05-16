d2afa43ead71106a9823a199251563402c730ae8
sandpiturtle <sandpiturtle@mail.ru>
2017-05-16 21:52:40 +0300

    Nice UI for method picking

--- OMCalc/CalculatorVC.swift
+++ OMCalc/CalculatorVC.swift
@@ -9,20 +9,29 @@
 import UIKit
 
 class CalculatorVC: UIViewController {
-  @IBOutlet weak var methodPicker: UIPickerView!
+  @IBOutlet weak var methodTextBox: UITextField!
+  @IBOutlet weak var methodDropDown: UIPickerView!
 
   override func viewDidLoad() {
     super.viewDidLoad()
-
-    methodPicker.dataSource = self
-    methodPicker.delegate = self
+    methodTextBox.text = methodsPickerData[0]
   }
 
   fileprivate let methodsPickerData = [ "Бисекция", "Золотое сечение", "Парабол", "Ньютона" ]
 }
 
-extension CalculatorVC: UIPickerViewDelegate, UIPickerViewDataSource {
+extension CalculatorVC: UIPickerViewDelegate {
+  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    view.endEditing(true)
+    return methodsPickerData[row]
+  }
 
+  func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    methodTextBox.text = methodsPickerData[row]
+  }
+}
+
+extension CalculatorVC: UIPickerViewDataSource {
   func numberOfComponents(in pickerView: UIPickerView) -> Int {
     return 1
   }
@@ -30,8 +39,15 @@ extension CalculatorVC: UIPickerViewDelegate, UIPickerViewDataSource {
   func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
     return methodsPickerData.count
   }
+}
 
-  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-    return methodsPickerData[row]
+extension CalculatorVC: UITextFieldDelegate {
+  func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+    let dropDownIsHidden = self.methodDropDown.isHidden
+    let duration = dropDownIsHidden ? 0.5 : 0.2
+    UIView.animate(withDuration: duration) {
+      self.methodDropDown.isHidden = !dropDownIsHidden
+    }
+    return false
   }
 }
